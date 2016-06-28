1431258811f125b67b1edac83fed43580ff70dbc
David Kumru <davidkumru@gmail.com>
2016-06-28 15:18:46 +0200

    change user name

--- app.rb
+++ app.rb
@@ -3,13 +3,13 @@ require 'httparty'
 require 'json'
 
 get '/refbot' do
-  postback params[:text], params[:channel_id], params[:user_id]
+  postback params[:text], params[:channel_id], params[:user_name]
   status 200
 end
 
 def postback message, channel
     slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => params[:user_id], "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+    HTTParty.post slack_webhook, body: {"text" => params[:user_name], "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
 end
 
 # get '/refbot' do
