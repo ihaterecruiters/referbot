1b519c40c447f68ce0fb0c571690db4aa8222ae3
David Kumru <davidkumru@gmail.com>
2016-06-28 16:30:49 +0200

    finalize postback

--- app.rb
+++ app.rb
@@ -2,11 +2,6 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
-# post '/refbot' do
-#   postback params[:text], params[:channel_id], params[:user_name]
-#   status 200
-# end
-
 post '/refbot' do
   input = params[:text].to_s.split(' ')
   case input[0].downcase
@@ -21,31 +16,18 @@ post '/refbot' do
 end
 
 def getlist
-  gettest = HTTParty.get('https://api.recruitee.com/c/levelupventures/careers/offers').to_json
+  receivelist = HTTParty.get('https://api.recruitee.com/c/levelupventures/careers/offers').to_json
 
-  test = JSON.parse(gettest, symbolize_names: true)
-  contents = test[:offers]
+  test1 = JSON.parse(receivelist, symbolize_names: true)
+  contents = test1[:offers]
 
   contents.each do |content|
-    # p content[:title]
-    # puts
-    # p content[:careers_url]
-    # puts
-
     postback content[:title], params[:channel_id], params[:user_name]
-
-    # slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    # HTTParty.post slack_webhook, body: {"text" => content[:title], "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
   end
 end
 
 
-def postback message, channel, user
-    slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => message, "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
-end
-
-# get '/refbot' do
+# def write_json
 #   jsoncontent = {"text" => "refbot response", "username" => "refbot", "channel" => params[:channel_id]}
 #   newjson = JSON.pretty_generate(jsoncontent)
 #   File.open("userdata.json","w") do |f|
@@ -53,3 +35,9 @@ end
 #   end
 #   newjson
 # end
+
+
+def postback message, channel, user
+    slack_webhook = ENV['SLACK_WEBHOOK_URL']
+    HTTParty.post slack_webhook, body: {"text" => message, "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+end
