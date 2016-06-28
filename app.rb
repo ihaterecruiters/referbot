4fc4c7a5651fdb74a17da592724d5ebb55646a6e
David Kumru <davidkumru@gmail.com>
2016-06-28 14:59:38 +0200

    test user name

--- app.rb
+++ app.rb
@@ -2,17 +2,21 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
-# get '/refbot' do
-#   postback params[:text], params[:channel_id]
-#   status 200
-# end
-#
-# def postback message, channel
-#     slack_webhook = ENV['SLACK_WEBHOOK_URL']
-#     HTTParty.post slack_webhook, body: {"text" => "testing", "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
-# end
-
 get '/refbot' do
-  jsoncontent = {"text" => "testing", "username" => "refbot", "channel" => params[:channel_id]}.to_json
-  jsoncontent
+  postback params[:text], params[:channel_id]
+  status 200
 end
+
+def postback message, channel
+    slack_webhook = ENV['SLACK_WEBHOOK_URL']
+    HTTParty.post slack_webhook, body: {"text" => params[:user_id], "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+end
+
+# get '/refbot' do
+#   jsoncontent = {"text" => "refbot response", "username" => "refbot", "channel" => params[:channel_id]}
+#   newjson = JSON.pretty_generate(jsoncontent)
+#   File.open("this.json","w") do |f|
+#     f.write(newjson)
+#   end
+#   newjson
+# end
