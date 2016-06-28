ec797c0a4552ada8ed991270ca409d29c18cb713
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-06-28 16:34:42 +0200

    Formatting Parsed Offers to Markdown

--- get_recruitee.rb
+++ get_recruitee.rb
@@ -1,1 +1,20 @@
-get_recruitee.rb
+require 'sinatra'
+require 'httparty'
+require 'json'
+
+class Recruitee
+  include HTTParty
+  format :json
+end
+
+gettest = Recruitee.get('https://api.recruitee.com/c/levelupventures/careers/offers').to_json
+
+test = JSON.parse(gettest, symbolize_names: true)
+contents = test[:offers]
+
+contents.each do |content|
+  p content [:id]
+  p content[:title]
+  p content[:careers_url]
+  #print "Offer ID: " content [:id] \n "Title: *bold*" content[:title]\n "URL: "[:careers_url]
+end
