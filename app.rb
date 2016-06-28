9a43cfb639b672c3673cd584531368990dd6bdfb
David Kumru <davidkumru@gmail.com>
2016-06-28 17:19:57 +0200

    add private postback

--- app.rb
+++ app.rb
@@ -6,10 +6,19 @@ post '/refbot' do
   input = params[:text].to_s.split(' ')
   case input[0].downcase
   when 'hello'
-    priv_postback "Hello " + params[:channel_id], params[:channel_id], params[:user_name]
+    priv_postback "Hello " + params[:user_name], params[:channel_id], params[:user_name]
     break
   when 'list'
     getlist
+    break
+  when '1'
+    userinput = 1
+    postback userinput, params[:channel_id], params[:user_name]
+    break
+  when '2'
+    postback userinput, params[:channel_id], params[:user_name]
+    userinput = 2
+    postback userinput, params[:channel_id], params[:user_name]
     break
   end
   status 200
@@ -44,5 +53,5 @@ end
 
 def priv_postback message, channel, user
     slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => message, "username" => "refbot", "channel" => "D1CUG5KU4" }.to_json, headers: {'content-type' => 'application/json'}
+    HTTParty.post slack_webhook, body: {"text" => message, "username" => "refbot", "channel" => params[:channel_id] }.to_json, headers: {'content-type' => 'application/json'}
 end
