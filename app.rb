7b8bc3cc04115717fb2423abee2a90d7434843b2
David Kumru <davidkumru@gmail.com>
2016-06-28 15:56:26 +0200

    revert settings

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ post '/refbot' do
   status 200
 end
 
-def postback message, channel
+def postback message, channel, user
     slack_webhook = ENV['SLACK_WEBHOOK_URL']
     HTTParty.post slack_webhook, body: {"text" => params[:user_name], "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
 end
