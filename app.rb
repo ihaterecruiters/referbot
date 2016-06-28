058c3b0e33e9a31cd64a357e6cb3193e1083f017
David Kumru <davidkumru@gmail.com>
2016-06-28 12:10:40 +0200

    added sinatra local code

--- app.rb
+++ app.rb
@@ -2,12 +2,17 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
-get '/refbot' do
-  postback params[:text], params[:channel_id]
-  status 200
-end
+# get '/refbot' do
+#   postback params[:text], params[:channel_id]
+#   status 200
+# end
+#
+# def postback message, channel
+#     slack_webhook = ENV['SLACK_WEBHOOK_URL']
+#     HTTParty.post slack_webhook, body: {"text" => "testing", "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+# end
 
-def postback message, channel
-    slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => "testing", "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+get '/refbot' do
+  jsoncontent = {"text" => "testing", "username" => "refbot", "channel" => params[:channel_id]}.to_json
+  jsoncontent
 end
