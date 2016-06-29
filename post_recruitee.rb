7a69195fb1e4524f6279adc921fc9ce55eed0137
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-06-29 09:45:32 +0200

    post-to-recruitee

--- post_recruitee.rb
+++ post_recruitee.rb
@@ -1,20 +1,23 @@
+require "sinatra"
+require "httparty"
+require 'json'
+
 class Partay
   include HTTParty
   base_uri 'http://localhost:3000'
 end
 
 options = {
-  body: {
-    candidate: { # your resource
-      name: 'Tony', # your columns/data
-      email: 'blabla@blabla.nl',
-      phone: '0692349851',
-      remote_cv_url: 'https://site.example.com/resumes/myresume.pdf'
-    }
-  },
-  headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+
 }
 
-options.to_json
 
-Partay.post('https://api.recruitee.com/c/referbot/careers/offers/designer-voorbeeld-vacature/candidates.json', options)
+Partay.post('https://api.recruitee.com/c/referbot/careers/offers/designer-voorbeeld-vacature/candidates', body: {
+  "candidate": {
+    "name": "Tony", 
+    "email": "blabla@blabla.nl",
+    "phone": "0692349851",
+    "remote_cv_url": "https://site.example.com/resumes/myresume.pdf"
+  }
+},
+headers: { 'Content-Type' => 'application/json'})
