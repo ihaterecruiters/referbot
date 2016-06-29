b5c820d70cd1fe5e222b2d30595dd86d1b1b464f
David Kumru <davidkumru@gmail.com>
2016-06-29 12:15:26 +0200

    add redis

--- app.rb
+++ app.rb
@@ -5,17 +5,17 @@ require 'redis'
 
 post '/refbot' do
 
-  # redis = Redis.new
+  redis = Redis.new
 
   # redis.set "foo", [1, 2, 3].to_json
-  # redis.set("mykey", "Hi from redis!")
-  # sentence = redis.get("mykey")
+  redis.set("mykey", "Hi from redis!")
+  sentence = redis.get("mykey")
   # redis_output = JSON.parse(redis.get("foo"))
 
   input = params[:text].to_s.split(' ')
   case input[0].downcase
   when 'hello'
-    postback "new", params[:channel_id], params[:user_name]
+    postback sentence, params[:channel_id], params[:user_name]
     break
   when 'list'
     getlist
