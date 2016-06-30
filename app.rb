0cc9751fcc3171da6b6201118240aeb4462dc927
Ben <ben@leipeshit.com>
2016-06-30 15:47:29 +0200

    local_test

--- app.rb
+++ app.rb
@@ -8,38 +8,43 @@ post '/refbot' do
   redis = Redis.new
 
   input = params[:text].to_s.split(' ')
-
+  message = ""
   case input[0].downcase
   when 'hello'
-    postback "Hello " + params[:user_name] + " welcome to referbot! Type /refbot help. for a list of all refbot keywords.", params[:channel_id], params[:user_name]
-    break
+    message = "Hello " + params[:user_name] + " welcome to referbot! Type /refbot help. for a list of all refbot keywords."
   when 'help'
-    postback "This is a list off all the commands: /refbot hello, /refbot help, /refbot list, /refbot new, /refbot new candidate first-name last-name email phone vacancy", params[:channel_id], params[:user_name]
-    break
+    message = "This is a list off all the commands: /refbot hello, /refbot help, /refbot list, /refbot new, /refbot new candidate first-name last-name email phone vacancy"
   when 'list'
-    getlist
-    break
+    message = getlist
+  # when 'new'
+  #   redis.hmset(input[1], "firstname", input[2], "lastname", input[3], "email", input[4], "phone", input[5], "vacancy", input[6])
+  #   # postback redis.hmget(input[1], "firstname", "lastname", "email", "phone", "vacancy").to_s, params[:channel_id], params[:user_name]
+  #
+  #   url = "https://api.recruitee.com/c/referbot/careers/offers/#{redis.hmget(input[1], "vacancy")[0].to_s}/candidates.json"
+  #   candidate = {
+  #     name: redis.hmget(input[1], "firstname")[0].to_s + " " + redis.hmget(input[1], "lastname")[0].to_s,
+  #     email: redis.hmget(input[1], "email")[0].to_s,
+  #     phone: redis.hmget(input[1], "phone")[0].to_s,
+  #     remote_cv_url: "http://cd.sseu.re/welcome-pdf.pdf"
+  #   }
+  #
+  #   HTTParty.post(url,
+  #     body: { candidate: candidate }.to_json,
+  #     headers: { "content-type" => "application/json" })
+  #
+  #     postback params[:user_name].to_s + " has just refered a new candidate for the following vacancy: https://referbot.recruitee.com/o/#{redis.hmget(input[1], "vacancy")[0].to_s}", params[:channel_id], params[:user_name]
+  #
+  #   status 200
+  # end
   end
 
-  if input[0].downcase == "new"
-    redis.hmset(input[1], "firstname", input[2], "lastname", input[3], "email", input[4], "phone", input[5], "vacancy", input[6])
-    # postback redis.hmget(input[1], "firstname", "lastname", "email", "phone", "vacancy").to_s, params[:channel_id], params[:user_name]
-
-    url = "https://api.recruitee.com/c/referbot/careers/offers/#{redis.hmget(input[1], "vacancy")[0].to_s}/candidates.json"
-    candidate = {
-      name: redis.hmget(input[1], "firstname")[0].to_s + " " + redis.hmget(input[1], "lastname")[0].to_s,
-      email: redis.hmget(input[1], "email")[0].to_s,
-      phone: redis.hmget(input[1], "phone")[0].to_s,
-      remote_cv_url: "http://cd.sseu.re/welcome-pdf.pdf"
-    }
-
-    HTTParty.post(url,
-      body: { candidate: candidate }.to_json,
-      headers: { "content-type" => "application/json" })
-
-      postback params[:user_name].to_s + " has just refered a new candidate for the following vacancy: https://referbot.recruitee.com/o/#{redis.hmget(input[1], "vacancy")[0].to_s}", params[:channel_id], params[:user_name]
-
-    status 200
+  json_message = {"text" => message, params[:user_name] => "refbot", "channel" => params[:channel_id]}
+  if ENV['DEV_ENV'] == 'test'
+    content_type :json
+   json_message.to_json
+  else
+    slack_webhook = ENV['SLACK_WEBHOOK_URL']
+    HTTParty.post slack_webhook, body: json_message.to_json, headers: {'content-type' => 'application/json'}
   end
 end
 
@@ -50,25 +55,9 @@ def getlist
 
   test1 = JSON.parse(receivelist, symbolize_names: true)
   contents = test1[:offers]
-
+  message = ""
   contents.each do |content|
-    message = "#{content[:id]}, #{content[:title]} \n #{content[:careers_url]}"
-    postback "The following vacancies are open:\n" + message, params[:channel_id], params[:user_name]
+    message = message + "#{content[:id]}, #{content[:title]} \n #{content[:careers_url]} \n"
   end
-end
-
-
-# def write_json
-#   jsoncontent = {"text" => "refbot response", "username" => "refbot", "channel" => params[:channel_id]}
-#   newjson = JSON.pretty_generate(jsoncontent)
-#   File.open("userdata.json","w") do |f|
-#     f.write(newjson)
-#   end
-#   newjson
-# end
-
-
-def postback message, channel, user
-    slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => message, "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+  return message;
 end
