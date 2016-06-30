97f48aa6162d26efcaee3deac7c6eeed50b787b6
David Kumru <davidkumru@gmail.com>
2016-06-30 11:26:17 +0200

    test post

--- app.rb
+++ app.rb
@@ -23,9 +23,9 @@ post '/refbot' do
 
   if input[0].downcase == "new"
     redis.hmset(input[1], "firstname", input[2], "lastname", input[3], "email", input[4], input[5], "phone", input[6], "vacancy")
-    postback redis.hmget(input[1], "firstname", "lastname", "email", "phone").to_s, params[:channel_id], params[:user_name]
+    postback redis.hmget(input[1], "firstname", "lastname", "email", "phone", "vacancy").to_s, params[:channel_id], params[:user_name]
 
-    url = "https://api.recruitee.com/c/referbot/careers/offers/#{redis.hmget(input[1], "vacancy")[0].to_s}/candidates.json"
+    url = 'https://api.recruitee.com/c/referbot/careers/offers/#{redis.hmget(input[1], "vacancy")[0].to_s}/candidates.json'
     # url = "https://api.recruitee.com/c/referbot/careers/offers/designer-voorbeeld-vacature/candidates.json"
     candidate = {
       name: redis.hmget(input[1], "firstname")[0].to_s + " " + redis.hmget(input[1], "lastname")[0].to_s,
