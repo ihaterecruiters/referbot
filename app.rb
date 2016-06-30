cb9d4cce46f028708d4a204d2180a794e1184363
David Kumru <davidkumru@gmail.com>
2016-06-30 15:31:04 +0200

    add user_id

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ post '/refbot' do
   end
 
   if input[0].downcase == "name" and redis.hmget(params[:user_id], "step")[0].to_s == "1"
-    redis.mapped_hmset(params[:user_id], "candidate_0"[:firstname] = input[1])
+    redis.hmset(params[:user_id], "candidate_0"[:firstname] = input[1])
     # redis.mapped_hmset(params[:user_id], "candidate_0"["lastname"] = input[2])
     # redis.mapped_hmset(params[:user_id], "step", "2")
     # firstname = redis.hmget(params[:user_id], "candidate_0"["firstname"])
