397e3ae05b6493fe2b13466249a5937ddbd18585
David Kumru <davidkumru@gmail.com>
2016-06-30 16:02:56 +0200

    add user_id

--- app.rb
+++ app.rb
@@ -38,13 +38,16 @@ post '/refbot' do
   end
 
   if input[0].downcase == "name" and redis.hmget(params[:user_id], "step")[0].to_s == "1"
-    redis.hmset(params[:user_id], "candidate_0"["firstname"], input[1])
+    # redis.hmset(params[:user_id], "candidate_0"["firstname"], input[1])
     # redis.mapped_hmset(params[:user_id], "candidate_0"["lastname"] = input[2])
     # redis.mapped_hmset(params[:user_id], "step", "2")
     # firstname = redis.hmget(params[:user_id], "candidate_0"["firstname"])
-    firstnamedb = redis.hmget(params[:user_id], "candidate_0")
+    # firstnamedb = redis.hmget(params[:user_id], "candidate_0")
 
-    postback "Name: " + firstnamedb[0]["firstname"].to_s, params[:channel_id], params[:user_name]
+    redis.mapped_hmset(params[:user_id], {"candidate_0": {firstname: input[1], lastname: "", email: "", phone: "", vacancy: ""}, "step": "1"})
+
+    firstnameget = redis.hmget(params[:user_id], "candidate_0"["firstname"])
+    postback "Name: " + firstnameget, params[:channel_id], params[:user_name]
   end
 
   # if input[0].downcase == "new"
