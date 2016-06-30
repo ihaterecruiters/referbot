d3617cdc0b823963958911d835072d518f72b66f
David Kumru <davidkumru@gmail.com>
2016-06-30 14:29:58 +0200

    add user_id

--- app.rb
+++ app.rb
@@ -26,7 +26,11 @@ post '/refbot' do
       redis.hmset(params[:user_id], "candidate_0", {firstname: "", lastname: "", email: "", phone: "", vacancy: ""}, "step", "1")
       postback params[:user_id] + " does not exist in the database. Creating...", params[:channel_id], params[:user_name]
     elsif redis.hmget(params[:user_id], "step")[0].to_s == "1"
+      redis.hmset(params[:user_id], "step", "2")
       postback params[:user_id] + " exists in the database. Adding candidate (step 1/6). Name: ", params[:channel_id], params[:user_name]
+    elsif redis.hmget(params[:user_id], "step")[0].to_s == "2"
+      redis.hmset(params[:user_id], "step", "3")
+      postback params[:user_id] + " Added name. Adding candidate (step 2/6). Email: ", params[:channel_id], params[:user_name]
     end
   end
 
