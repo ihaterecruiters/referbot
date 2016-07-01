e2d2e429940496e2a315decc18e1fbc71738a361
David Kumru <davidkumru@gmail.com>
2016-07-01 12:16:14 +0200

    copied

--- app.rb
+++ app.rb
@@ -4,110 +4,80 @@ require 'json'
 require 'redis'
 
 post '/refbot' do
-
-  redis = Redis.new
-
+  $redis = Redis.new
+  candidate_content = $redis.hmget(params[:user_id], "candidate_0")
   input = params[:text].to_s.split(' ')
-
+  message = ""
   case input[0].downcase
   when 'hello'
-    # currentpost
-    postback "Hello " + params[:user_name] + " welcome to referbot! Type /refbot help. for a list of all refbot keywords.", params[:channel_id], params[:user_name]
-    break
+    message = "Hello " + params[:user_name] + " welcome to referbot! Type /refbot help. for a list of all refbot keywords."
+    notification = checklist
   when 'help'
-    postback "This is a list off all the commands: /refbot hello, /refbot help, /refbot list, /refbot new, /refbot new candidate name email phone vacancy", params[:channel_id], params[:user_name]
-    break
+    message = "This is a list off all the commands: /refbot hello, /refbot help, /refbot list, /refbot new, /refbot new candidate first-name last-name email phone vacancy"
+    notification = checklist
   when 'list'
-    getlist
-    break
-  end
+    message = getlist
+    notification = checklist
 
-  if input[0].downcase == "new"
-    if !redis.exists(params[:user_id])
-      redis.mapped_hmset(params[:user_id], {"candidate": {name: "", email: "", phone: "", vacancy: ""}, "step": "1"})
-
-      postback params[:user_id] + " profile does not exist in the database. Created... \n Type '/refbot name <candidate name>' to start adding a new candidate. Step 1/5.", params[:channel_id], params[:user_name]
-    elsif redis.exists(params[:user_id])
-      postback params[:user_id] + " profile exists in the database. \n Type '/refbot name <candidate name>' to start adding a new candidate. Step 1/5.", params[:channel_id], params[:user_name]
+  when "new"
+    if !$redis.exists(params[:user_id])
+      $redis.mapped_hmset(params[:user_id], {"candidate_0": {firstname: "", lastname: "", email: "", phone: "", vacancy: ""}, "step": "1"})
+      message = params[:user_id] + " profile does not exist in the database. Created. Type '/refbot name <candidate name>' to start adding a new candidate."
+    elsif $redis.exists(params[:user_id])
+      message = params[:user_id] + " profile exists in the database. Type '/refbot name <candidate name>' to start adding a new candidate. Step 1/5."
     end
+
+  when "reset"
+    $redis.mapped_hmset(params[:user_id], {"candidate_0": {firstname: "", lastname: "", email: "", phone: "", vacancy: ""}, "step": "1"})
   end
 
-  if input[0].downcase == "reset"
-    redis.mapped_hmset(params[:user_id], {"candidate": {name: "", email: "", phone: "", vacancy: ""}, "step": "1"})
-    postback "Candidate creation reset.", params[:channel_id], params[:user_name]
+  if input[0].downcase == "name" and eval(candidate_content[0])[:firstname].to_s == ""
+    $redis.mapped_hmset(params[:user_id], {"candidate_0": {firstname: input[1], lastname: "", email: "", phone: "", vacancy: ""}, "step": "2"})
+    firstnameget = $redis.hmget(params[:user_id], "candidate_0")
+    message = "Added name: " + eval(firstnameget[0])[:firstname].to_s + ". Type '/refbot email <candidate email>' to add an email address. Step 2/5."
+  elsif input[0].downcase == "name" and eval(candidate_content[0])[:firstname].to_s != ""
+    $redis.mapped_hmset(params[:user_id], {"candidate_0": {firstname: input[1], lastname: "", email: "", phone: "", vacancy: ""}, "step": "2"})
+    firstnameget = $redis.hmget(params[:user_id], "candidate_0")
+    message = "Changed name to: " + eval(firstnameget[0])[:firstname].to_s + ". Type '/refbot email <candidate email>' to add an email address. Step 2/5."
+
   end
 
-  if input[0].downcase == "name"
-    redis.mapped_hmset(params[:user_id], {"candidate": {name: input[1..-1].join(" "), email: "", phone: "", vacancy: ""}, "step": "2"})
+  json_message = {"text" => message, params[:user_name] => "refbot", "channel" => params[:channel_id]}
+  if ENV['DEV_ENV'] == 'test'
+    content_type :json
+    x = "#{json_message[:text]}"
+   json_message[:text] = "#{message} + #{notification}"
+   json_message.to_json
+  else
+    slack_webhook = ENV['SLACK_WEBHOOK_URL']
+    notif_message = {"text" => notification, params[:user_name] => "refbot", "channel" => params[:channel_id]}
+    HTTParty.post slack_webhook, body: json_message.to_json, headers: {'content-type' => 'application/json'}
+    HTTParty.post slack_webhook, body: notif_message.to_json, headers: {'content-type' => 'application/json'}
 
-    postback "New name: " + eval(redis.hmget(params[:user_id], "candidate")[0])[:name].to_s + ". \n Type '/refbot email <candidate email>' to add an email address. Step 2/5.", params[:channel_id], params[:user_name]
   end
-
-  if input[0].downcase == "email" and eval(redis.hmget(params[:user_id], "candidate")[0])[:name].to_s != ""
-    redis.mapped_hmset(params[:user_id], {"candidate": {name: eval(redis.hmget(params[:user_id], "candidate")[0])[:name].to_s, email: input[1..-1].join(" "), phone: "", vacancy: ""}, "step": "3"})
-
-    postback "New email for " + eval(redis.hmget(params[:user_id], "candidate")[0])[:name].to_s + ": " + eval(redis.hmget(params[:user_id], "candidate")[0])[:email].to_s + ". \n Type '/refbot phone <candidate phone>' to add a phone number. Step 3/5.", params[:channel_id], params[:user_name]
-  elsif input[0].downcase == "email" and eval(redis.hmget(params[:user_id], "candidate")[0])[:name].to_s == ""
-    postback "First add a name using '/refbot name <candidate name>'", params[:channel_id], params[:user_name]
-  end
-
 end
-
-
-
-
-  # if input[0].downcase == "new"
-  #   redis.hmset(input[1], "firstname", input[2], "lastname", input[3], "email", input[4], "phone", input[5], "vacancy", input[6])
-  #   # postback redis.hmget(input[1], "firstname", "lastname", "email", "phone", "vacancy").to_s, params[:channel_id], params[:user_name]
-  #
-  #   url = "https://api.recruitee.com/c/referbot/careers/offers/#{redis.hmget(input[1], "vacancy")[0].to_s}/candidates.json"
-  #   candidate = {
-  #     name: redis.hmget(input[1], "firstname")[0].to_s + " " + redis.hmget(input[1], "lastname")[0].to_s,
-  #     email: redis.hmget(input[1], "email")[0].to_s,
-  #     phone: redis.hmget(input[1], "phone")[0].to_s,
-  #     remote_cv_url: "http://cd.sseu.re/welcome-pdf.pdf"
-  #   }
-  #
-  #   HTTParty.post(url,
-  #     body: { candidate: candidate }.to_json,
-  #     headers: { "content-type" => "application/json" })
-  #
-  #     postback params[:user_name].to_s + " has just refered a new candidate for the following vacancy: https://referbot.recruitee.com/o/#{redis.hmget(input[1], "vacancy")[0].to_s}", params[:channel_id], params[:user_name]
-  #
-  #   status 200
-  # end
-
 
 def getlist
   receivelist = HTTParty.get('https://api.recruitee.com/c/referbot/careers/offers').to_json
   # receivelist = HTTParty.get('https://api.recruitee.com/c/levelupventures/careers/offers').to_json
-
   test1 = JSON.parse(receivelist, symbolize_names: true)
   contents = test1[:offers]
-
+  message = ""
   contents.each do |content|
-    message = "#{content[:id]}, #{content[:title]} \n #{content[:careers_url]}"
-    postback message, params[:channel_id], params[:user_name]
+    message = message + "#{content[:id]}, #{content[:title]} \n #{content[:careers_url]} \n"
   end
+  return message;
 end
 
-
-def currentpost
-  test1 = JSON.parse(receivelist, symbolize_names: true)
+def checklist
+  check_list = HTTParty.get('https://api.recruitee.com/c/referbot/careers/offers').to_json
+  test1 = JSON.parse(check_list, symbolize_names: true)
   contents = test1[:offers]
-
-  current_post = []
-
-  contents.each do |content|
-    current_post << content[:id]
-
+  message = ""
+  if $redis.get('lists') != contents.size.to_s
+    message = 'vacancy list has been updated'
+    $redis.set('lists', contents.size.to_s)
   end
-  postback current_post.to_s, params[:channel_id], params[:user_name]
-  redis.set post current_post.count
-end
-
-
-def postback message, channel, user
-    slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => message, "username" => "refbot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+  #message = $redis.get("listsize")
+  return message
 end
